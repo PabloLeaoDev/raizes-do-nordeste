ad406c4c6b7aa3e5ce04958090be1c4ceb2d1d6f
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2026-05-10 17:49:29 -0300

    delete dump files

--- src/infra/providers/payment.mock.ts
+++
@@ -1,9 +0,0 @@
-export async function processPaymentMock() {
-    const success = Math.random() > 0.2;
-
-    return {
-        success,
-        transactionId: Date.now(),
-        timestamp: new Date()
-    };
-}

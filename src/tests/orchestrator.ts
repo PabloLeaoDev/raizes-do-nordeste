4321c267dc80af21b4c5a20a85c088e6fbc8b33e
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2026-05-11 22:13:28 -0300

    update `orchestrator.ts`

--- src/tests/orchestrator.ts
+++ src/tests/orchestrator.ts
@@ -3,7 +3,7 @@ import retry from "async-retry";
 async function waitForAllServices() {
   const fetchStatusPage = async () => {
     try {
-      const response = await fetch("http://localhost:3000/api/v1/status");
+      const response = await fetch(`${process.env.HOST || 'http://localhost'}:${process.env.PORT || 3000}/`);
       if (!response.ok) throw Error(`HTTP Error ${response.status}`);
       await response.json();
     } catch (err) {

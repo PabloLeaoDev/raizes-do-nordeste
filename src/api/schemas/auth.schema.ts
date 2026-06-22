9486c6d6a45a8ed0a27d34b884682fc3ef4ac7d9
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2026-06-22 00:21:45 -0300

    refactor: removes `Zod` deprecated string based email method

--- src/api/schemas/auth.schema.ts
+++ src/api/schemas/auth.schema.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email(),
+  email: z.email(),
   senha: z.string().min(6),
 });
 
 export const signupSchema = z.object({
   nome: z.string().min(3),
-  email: z.string().email(),
+  email: z.email(),
   senha: z.string().min(6),
   perfil: z
     .enum(["ADMIN", "GERENTE", "ATENDENTE", "CLIENTE"])

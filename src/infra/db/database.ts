a3179768cf32d3145f3d92f68c4547e4263f58d1
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2026-07-26 14:19:42 -0300

    refactor: updates `database.ts` with `query`, `getNewClient` and `getSSLValues` functions

--- src/infra/db/database.ts
+++ src/infra/db/database.ts
@@ -1,14 +1,53 @@
-import { Pool } from "pg";
+import { Client } from "pg";
+import { ServiceError } from "@src/infra/errors";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "5432"),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+async function query(queryObject: { text: string; values?: any[] }) {
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      message: "Database connection or query error",
+      cause: error as Error,
+    });
+    throw serviceErrorObject;
+  } finally {
+    await client?.end();
+  }
+}
 
-export default pool;
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "5432"),
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production";
+}
+
+const database = {
+  query,
+  getNewClient,
+};
+
+export default database;
